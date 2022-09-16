e494ea03de4b9f18b803040ffd9a717807c6912b
BuildTools <unconfigured@null.spigotmc.org>
2022-09-17 04:16:45 +0430

    Added test comment

--- src/main/java/net/cagedinrage/tst1/TestMod.java
+++ src/main/java/net/cagedinrage/tst1/TestMod.java
@@ -3,7 +3,7 @@ package net.cagedinrage.tst1;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+// test comment
 public class TestMod implements ModInitializer {
 	public static final String modid = "tst1";
 	public static final Logger LOGGER = LoggerFactory.getLogger(modid);
