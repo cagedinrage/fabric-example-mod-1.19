237634a5c539ca1805959eac735af7da84cf4520
BuildTools <unconfigured@null.spigotmc.org>
2022-09-17 04:08:57 +0430

    Initial commit

---
+++ src/main/java/net/cagedinrage/tst1/TestMod.java
@@ -0,0 +1,15 @@
+package net.cagedinrage.tst1;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestMod implements ModInitializer {
+	public static final String modid = "tst1";
+	public static final Logger LOGGER = LoggerFactory.getLogger(modid);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
